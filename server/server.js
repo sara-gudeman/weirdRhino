b7ba94a9b064b61f0c58599087624b166a380686
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-08-28 11:05:26 -0700

    Server sends index.html - closes #26

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 var express = require('express');
-var bodyParser = ('body-parser');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '../client/static'));
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
 
 app.listen(port);
 console.log('listening on ' + port);
