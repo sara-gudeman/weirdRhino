611eddb65a967772a8c4f419a1c53fc50940eade
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-29 15:47:11 -0700

    Store results of interrogation in products collection

--- server/tasks/wrapper.js
+++ server/tasks/wrapper.js
@@ -1,5 +1,6 @@
 var wappalyzer = require('wappalyzer');
 var url = require('url');
+var Product = require('../db/products/productModel');
 
 module.exports = function(site) {
   var options = {
@@ -12,6 +13,16 @@ module.exports = function(site) {
      * apps is an array of apps
      * appInfo is a much deeper dive into each app
      */ 
+    var currentProduct = new Product({
+      product_name: url.parse(site)['host'],
+      product_technologies: apps,
+      scrape_date: Date.now(),   
+      product_url: site
+    });
+
+    currentProduct.save(function(err) {
+      if(err) {console.log(err);};
+    }); 
   });
 };
 
