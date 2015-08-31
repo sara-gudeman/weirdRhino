0392aa6546174a2c6bc78a8886860776d9d79526
Harry Sadler <harrysadlermusic@gmail.com>
2015-08-31 16:56:03 -0700

    add comma delineated search to products API, add underscore as a node module

--- server/products/productController.js
+++ server/products/productController.js
@@ -1,21 +1,30 @@
 var Product = require('./productModel');
+var _ = require('underscore');
 
 module.exports = {
 
   searchTech: function(req, res) {
-    var searchRegex = new RegExp(req.query.searchString + '.*', 'i');
+    // get search terms
+    var searchTerms = req.body.searchString.split(',');
+    // construct object array for DB query
+    // trim whitespace and convert to regex
+    var toSearch = _.map(searchTerms, function(str, index) {
+      return {
+        product_technologies: new RegExp(str.trim() + '.*', 'i')
+      };
+    });
     console.log('search request received..');
-    console.log('req.query: ', req.query);
-    console.log('search regex: ', searchRegex);
+    console.log('searchString: ----------------------->', req.body.searchString);
+    console.log('toSearch: ----------------------->', toSearch);
 
-    Product.find({product_technologies: searchRegex}, function(error, result) {
+    // use toSearch to query the DB
+    Product.find({$or: toSearch}, function(error, result) {
       if(error) {
         res.sendStatus(500);
       }
       console.log(result);
       res.send(JSON.stringify(result));
     });
-
   }
 
 };
