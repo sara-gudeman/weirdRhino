fb003b29bcd69ed0853fbbc190ec47b705d8309e
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-01 14:49:24 -0700

    Added fields to technology model

--- server/technologies/technologyModel.js
+++ server/technologies/technologyModel.js
@@ -2,7 +2,9 @@ var Sequelize = require('sequelize');
 var Product = require('../products/productModel');
 
 var Technology = Sequelize.define('Technology', {
-
+  technology_name: Sequelize.STRING,
+  site: Sequelize.STRING,
+  category: Sequelize.STRING
 });
 
 Technology.belongsTo(Product);
