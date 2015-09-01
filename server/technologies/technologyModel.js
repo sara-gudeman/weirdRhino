5d9f93e868dfcd87c66a5fa777bffb09cf8ee46d
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-01 16:51:17 -0700

    Require the sequelize db connection in techmodel

--- server/technologies/technologyModel.js
+++ server/technologies/technologyModel.js
@@ -1,10 +1,13 @@
-var Sequelize = require('sequelize');
+var Sequelize = require('../db/database');
 var Product = require('../products/productModel');
 
+/**
+ * TODO: Make some of the fields required
+ */
 var Technology = Sequelize.define('Technology', {
   technology_name: Sequelize.STRING,
   site: Sequelize.STRING,
-  category: Sequelize.STRING
+  category: Sequelize.STRING,
 });
 
 Technology.belongsTo(Product);
