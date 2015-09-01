270f94206d43e483ce613f844acff74497cdfeaa
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-09-01 16:40:16 -0700

    Components now use map in render function - closes #84

--- client/src/components/ResultItem.jsx
+++ client/src/components/ResultItem.jsx
@@ -2,20 +2,12 @@ var ResultItem = React.createClass({
 
   // placeholder click handler for result items
   handleResultClick: function() {
-    console.log('list item clicked');
   },
 
   render: function() {
-
-    // for loop because map was not available
-      // TODO: make map work?
-    var techs = '';
-    var updateTechs = function(tech) {
-      techs = techs + tech + ' ';
-    }
-    for (var i = 0; i < this.props.techList.length; i ++) {
-      updateTechs(this.props.techList[i]);
-    }
+    var techs = this.props.techList.map(function(tech) {
+      return tech + ' ';
+    });
     return (
       <div className="result-item" onClick={this.handleResultClick}>
         <h3>{this.props.name}</h3>
