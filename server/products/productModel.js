b431f521873e48f642674060c4860f0b6f5ff3ac
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-01 16:50:55 -0700

    Require the sequelize connection

--- server/products/productModel.js
+++ server/products/productModel.js
@@ -1,10 +1,10 @@
-var Sequelize = require('sequelize');
+var Sequelize = require('../db/database');
 var Technology = require('../technologies/technologyModel');
 
 /**
  * TODO: Make some of the fields required.
  */
-var Product = Sequelize.define('Product',{
+var Product = Sequelize.define('Product', {
   product_name: Sequelize.STRING,
   scrape_date: Sequelize.DATE,
   product_url: Sequelize.STRING,
