3e634eb397438ebfe37cd49c05fd90f940fc40e8
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-01 12:25:09 -0700

    Hooked up MySQL DB connection

--- server/db/database.js
+++ server/db/database.js
@@ -1,16 +1,3 @@
-var mongoose = require('mongoose');
+var Sequelize = require('sequelize');
 
-var mongoURI = 'mongodb://localhost/stackmatch';
-console.log('CONNECTED TO:', mongoURI);
-
-mongoose.connect(mongoURI);
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function(cb) {
-  console.log('MongoDB connection open');
-});
-
-module.exports = db;
+module.exports = new Sequelize('mysql://root@localhost:3306/stackmatch');
