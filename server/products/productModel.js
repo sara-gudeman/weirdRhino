a7734e3e269d28be4b64fab995f222ad977998b6
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-02 15:52:08 -0700

    Product model plays nice with new model flow

--- server/products/productModel.js
+++ server/products/productModel.js
@@ -1,15 +1,13 @@
-var db = require('../db/models').sequelize;
-var Sequelize = require('sequelize');
-
 /**
  * TODO: Make some of the fields required.
  */
-var Product = db.define('Product', {
-  product_name: Sequelize.STRING,
-  scrape_date: Sequelize.DATE,
-  product_url: Sequelize.STRING,
-});
+module.exports = function(sequelize, Sequelize) {
+  var Product = sequelize.define('Product', {
+    product_name: Sequelize.STRING,
+    scrape_date: Sequelize.DATE,
+    product_url: Sequelize.STRING,
+  });
 
-Product.sync(); 
-
-module.exports = Product;
+  Product.sync(); 
+  return Product;
+}
