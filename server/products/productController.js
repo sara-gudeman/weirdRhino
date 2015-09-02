c6e0c237e08ee92e9cd9264270ea5c76b190a47b
Harry Sadler <harrysadlermusic@gmail.com>
2015-09-01 17:04:33 -0700

    update searchTech endpoint to return zero product results when searchString is empty

--- server/products/productController.js
+++ server/products/productController.js
@@ -6,27 +6,33 @@ module.exports = {
   searchTech: function(req, res) {
     // get search terms
     console.log('post request received...');
-    console.log(req.body);
-    var searchTerms = req.body.searchString.split(',');
-    // construct object array for DB query
-    // trim whitespace and convert to regex
-    var toSearch = _.map(searchTerms, function(str, index) {
-      return {
-        product_technologies: new RegExp(str.trim() + '.*', 'i')
-      };
-    });
-    console.log('search request received..');
-    console.log('searchString: ----------------------->', req.body.searchString);
-    console.log('toSearch: ----------------------->', toSearch);
 
-    // use toSearch to query the DB
-    Product.find({$or: toSearch}, function(error, result) {
-      if(error) {
-        res.sendStatus(500);
-      }
-      console.log(result);
-      res.send(JSON.stringify(result));
-    });
+    // return empty array if search string is an empty string
+    if(req.body.searchString === '') {
+      res.send(JSON.stringify([]));
+    }
+    // else, do a normal search
+    else {
+      var searchTerms = req.body.searchString.split(',');
+      // construct object array for DB query
+      // trim whitespace and convert to regex
+      var toSearch = _.map(searchTerms, function(str, index) {
+        return {
+          product_technologies: new RegExp(str.trim() + '.*', 'i')
+        };
+      });
+      console.log('search request received..');
+      console.log('searchString: ----------------------->', req.body.searchString);
+      console.log('toSearch: ----------------------->', toSearch);
+
+      // use toSearch to query the DB
+      Product.find({$or: toSearch}, function(error, result) {
+        if(error) {
+          res.sendStatus(500);
+        }
+        res.send(JSON.stringify(result));
+      });
+    }
   }
 
 };
