2350e3300bbfa065e052b9aa8c897ff7e97eae53
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-02 19:30:49 -0700

    Cleaned up company model

--- server/companies/companyModel.js
+++ server/companies/companyModel.js
@@ -1,10 +1,7 @@
 module.exports = function(sequelize, Sequelize) {
-  var Company = sequelize.define('Company' {
+  return sequelize.define('Company', {
     company_name: Sequelize.STRING,
     company_location: Sequelize.STRING,
     company_url: Sequelize.STRING
   });
-
-  //Company.sync();
-  return Company;
 }
