eee40b8040601f97356534668f9192c38a617670
Harry Sadler <harrysadlermusic@gmail.com>
2015-09-02 20:41:14 -0700

    add search product by name API

--- server/products/productController.js
+++ server/products/productController.js
@@ -33,6 +33,27 @@ module.exports = {
         res.send(JSON.stringify(result));
       });
     }
+  },
+
+  searchProductName: function(req, res) {
+    console.log('products GET request received...');
+
+    // get the product name from the query string
+    var productName = req.query.name;
+    console.log('product name: ----------------------> ', productName);
+
+    // use the product name to find a single result in the DB
+    Product.findOne({product_name: productName}, function(error, result) {
+      if(error) {
+        res.sendStatus(500);
+      }
+      res.send(JSON.stringify(result));
+    });
   }
 
 };
+
+
+
+
+
--- server/products/productRoutes.js
+++ server/products/productRoutes.js
@@ -2,4 +2,5 @@ var ProductController = require('./productController');
 
 module.exports = function(route) {
   route.post('/', ProductController.searchTech);
+  route.get('/', ProductController.searchProductName);
 };
