7e2bbfdc6ee7a8358d9e5d749dcb32c57ce88194
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-02 19:31:04 -0700

    Cleaned up product model

--- server/products/productModel.js
+++ server/products/productModel.js
@@ -2,12 +2,9 @@
  * TODO: Make some of the fields required.
  */
 module.exports = function(sequelize, Sequelize) {
-  var Product = sequelize.define('Product', {
+  return sequelize.define('Product', {
     product_name: Sequelize.STRING,
     scrape_date: Sequelize.DATE,
     product_url: Sequelize.STRING,
   });
-
-  //Product.sync(); 
-  return Product;
 }
