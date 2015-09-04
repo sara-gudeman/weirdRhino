f6e3ae82d721763904a6dca2a9a5325343eb94c3
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-04 16:10:12 -0700

    Started rebuilding scraper using modularity

---
+++ server/tasks/spliceQueueAndProducts.js
@@ -0,0 +1,24 @@
+var Promise = require('bluebird');
+var models = require('../db/models');
+var Product = models.Product;
+var Technology = models.Technology;
+var siteQueue = require('./siteQueue');
+
+module.exports = function() {
+  return new Promise(function(resolve, reject) {
+    Product.findAll()
+      .then(function(products){
+        for(var i = 0; i < products.length; i++) {
+          if(siteQueue.indexOf(products[i].product_url === -1)) {
+            siteQueue.push(products[i].product_url);
+          }
+        }
+
+        resolve(siteQueue);
+      })
+      .catch(function(e) {
+        console.log("Error combinging existing Products and new sites");
+        reject(new Error("Error combining existing Products and new sites"));
+      });
+  });
+}
