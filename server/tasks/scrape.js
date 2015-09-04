7c674cf7fd64fa15110bf0692bb9261b68482459
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-04 16:09:10 -0700

    STarting over with scrapejs

--- server/tasks/scrape.js
+++ server/tasks/scrape.js
@@ -1,52 +1,20 @@
 var wrapp = require('./wrapper');
-var fs = require('fs');
 var url = require('url');
 var Promise = require('bluebird');
+var request = require('request');
 var siteQueue = require('./siteQueue');
 var models = require('../db/models');
 var Product = models.Product;
-/**
- * Maybe server hasn't spun up connection to db
- */
+var spliceQueueAndProducts = require('./spliceQueueAndProducts');
+Promise.promisifyAll(request);
 
-var completed = 0;
-var total;
-var runTime = 0;
-console.log("About to find things");
-
-/**
- * Set a timeout b/c batch scanning often leads to a non
- * 100% success rate.
- * Wait 5 minutes and if the scan is more than 95% complete,
- * then let's call it a day by closing out the database
- * connection and callying a good all process.exit(0);
- */
-setInterval(function() {
-  runTime += 20;
-  console.log("---Info Gathering has taken " + runTime + " seconds---");
-  if(runTime > total) {
-    console.log("[!!!] Scan has reached 95% success and run overtime. ");
-    console.log("[!!!] Shutting down the scan and closing DB connection.");
-    process.exit(0);
-  }
-}, 20000);
-
-Product.findAll()
-  .then(function(results) {
-    for(var i = 0; i < results.length; i++) {
-      /** Only push unique sites onto queue */
-      if(siteQueue.indexOf(results[i].product_url) === -1) {
-        siteQueue.push(results[i].product_url);
-      }
-    }
-    total = siteQueue.length;
-    console.log("Estimated run time " + total + " seconds");
-    return siteQueue;
-  })
-  .then(function(queue) {
-    for(var i = 0; i < queue.length; i++) {
-      wrapp(queue[i]);
-    }
-  });
+var requestArray = [];
 
 
+spliceQueueAndProducts()
+.then(function(results) {
+  console.log(results);
+})
+.catch(function(e) {
+  console.log(e);
+});
