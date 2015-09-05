dba7884e7091f8b16cfb9b504b95ed2cc9ada778
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-04 19:55:14 -0700

    Added module get technologies

---
+++ server/tasks/getTechnologies.js
@@ -0,0 +1,16 @@
+var models = require('./db/models');
+var Technology = models.Technology;
+var apps = require('./apps');
+
+module.exports = function(wappedProducts) {
+  var techModels = []
+  for(var tech in apps.apps) {
+    techModels.push(Technology.findOrCreate({
+      where: {
+        technology_name: tech
+        site: tech.website 
+      })
+    });
+  }
+  return [wappedProducts, techModels];
+}
