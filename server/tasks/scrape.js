066594fc9329d289f12d9ac015772aded162e798
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-04 19:54:16 -0700

    Further along on the scraper

--- server/tasks/scrape.js
+++ server/tasks/scrape.js
@@ -2,13 +2,24 @@ var Promise = require('bluebird');
 var siteQueue = require('./siteQueue');
 var models = require('../db/models');
 var Product = models.Product;
+var Technology = models.Technology;
 var spliceQueueAndProducts = require('./spliceQueueAndProducts');
-var makeRequests = require('./makeRequests');
-var wappResponses = require('./wappResponses');
+var wappProducts = require('./wappProducts');
+var toProductModels = require('./toProductModels');
+var associateProductsWithTech = require('./associateProductsWithTech');
+var getTechnologies = require('./getTechnologies');
 
 spliceQueueAndProducts()
-.then(makeRequests)
-.settle(wappResponses)
+.then(toProductModels)
+.settle()
+.then(wappProducts)
+.settle()
+.then(getTechnologies)
+.settle()
+.then(console.log)
 .catch(function(e) {
   console.log(e);
-});
+})
+.finally(function() {
+  console.log("All done!")
+})
