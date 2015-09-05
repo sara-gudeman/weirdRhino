5c4257eed1fec86c880be53ed2672f40cac6f58b
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-04 21:06:30 -0700

    Everything works except associations aren't saved in join table

--- server/tasks/associateProductsWithTech.js
+++ server/tasks/associateProductsWithTech.js
@@ -1,17 +1,41 @@
-var models = require('../db/models');
+var Promise = require('bluebird');
 
 module.exports = function(productTechTuple) {
-  var p = productTechTuple[0].value();
-  var t = productTechTuple[1].value();
+  var products = productTechTuple[0].value();
+  var technologies = productTechTuple[1].value();
   
-  var products = p.map(function(product) {
+  return Promise.map(products, function(product) {
     var productModel = product.value()[0];
     var productApps = product.value()[1];
+    var appsToAssociate = [];
 
-    return [productModel, productApps];
+    /**
+     * Keep from executing below associations with
+     * no apps to associate. Would likely throw
+     * and error, but will definitely save a little
+     * time and a DB call
+     */
+    if(productApps.length <= 0) {
+      return "None to associate";
+    }
+
+    for(var i = 0; i < productApps.length; i++) {
+      appsToAssociate.push(technologies.filter(function(tech) {
+       tech.technology_name = productApps[i]
+      })[0]);
+    }
+
+    for(var i = 0; i < appsToAssociate; i++) {
+      productModel.setTechnologies(appsToAssociate[i]);
+    }
+
+    productModel.scrape_date = Date.now();
+    return productModel.save()
+    .then(function(model) {
+      console.log(model)
+    })
+    .catch(function(e) {
+      console.log("Error saving model: ", e);
+    });
   });
-
-
-
- // console.log([products, technologies]);
 }
