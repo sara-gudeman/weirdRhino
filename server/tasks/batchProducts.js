78aa2e66d54f5ca6011f88802665b71f32b74804
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-05 16:58:35 -0700

    Added ability to set variable batch size

--- server/tasks/batchProducts.js
+++ server/tasks/batchProducts.js
@@ -1,23 +1,24 @@
 /**
- * Break all of the urls into batches of ten.
+ * Break all of the urls into batches of BATCH_SIZE.
  * This reduces the concurrent workload on node,
  * and helps keep variables stable.
  *
  * @param Array productModels
  * @return Array of arrays
  */
+var BATCH_SIZE = 1;
 module.exports = function(productModels) {
   var results = [];
   var batch = [];
 
   for(var i = 0; i < productModels.length; i++) {
-    //if((i !== 0 && i % 10 === 0) || i === productModels.length) {
+    if((i !== 0 && i % BATCH_SIZE === 0) || i === productModels.length) {
       results.push([productModels[i]]);
-   //   batch = [];
-   //   batch.push(productModels[i]);
-   // } else {
-   //   batch.push(productModels[i]);
-   // }
+      batch = [];
+      batch.push(productModels[i]);
+    } else {
+      batch.push(productModels[i]);
+    }
   }
 
   return results;
