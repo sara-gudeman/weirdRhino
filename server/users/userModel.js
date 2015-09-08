ca08d02252e3ce4731717fd700abbb0a2595995e
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-08 11:26:33 -0700

    Added token storage in user field

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,6 +1,7 @@
 module.exports = function(sequelize, Sequelize) {
   return sequelize.define('User', {
     username: Sequelize.STRING,
-    hashed_password: Sequelize.STRING
+    hashed_password: Sequelize.STRING,
+    token: Sequelize.STRING
   });
 }
