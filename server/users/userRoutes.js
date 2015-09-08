53ed81a73217d16b1fb7535c2daf20e3e5a2a312
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-08 11:58:38 -0700

    Hooked up api/users/:username to getUser

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -3,4 +3,5 @@ var UserController = require('./userController');
 module.exports = function(route) {
   route.post('/login', UserController.login);
   route.post('/signup', UserController.signup);
+  route.get('/:username', UserController.getUser);
 };
