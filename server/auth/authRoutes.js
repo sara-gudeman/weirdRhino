efa113ed9902eec1eda4d3bb19a085d082babe0e
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-07 19:03:00 -0700

    Moved auth stuff into users

--- server/auth/authRoutes.js
+++
@@ -1,6 +0,0 @@
-var AuthController = require('./authController');
-
-module.exports = function(route) {
-  route.post('/login', AuthController.userLogin);
-  route.post('/signup', AuthController.userSignup);
-};
