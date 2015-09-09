2ab65da595f249ee1ec76870beb84ce7556e75e7
SARA GUDEMAN <sgudeman982@gmail.com>
2015-09-08 21:27:24 -0700

    (feat)sets up api routing w/out db queries

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,11 +3,14 @@ module.exports = function (app, express) {
   // Express 4 allows us to use multiple routers with their own configurations
   var productRouter = express.Router();
   var authRouter = express.Router();
+  var techRouter = express.Router();
 
   app.use('/api/products', productRouter);
   app.use('/api/users', authRouter);
+  app.use('/api/technologies', techRouter);
 
   // inject our routers into their respective route files
   require('../products/productRoutes.js')(productRouter);
   require('../users/userRoutes.js')(authRouter);
+  require('../technologies/technologyRoutes.js')(techRouter);
 };
