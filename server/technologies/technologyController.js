7857839fa7b9dad8ee3327ee9e17e93ea5b11217
SARA GUDEMAN <sgudeman982@gmail.com>
2015-09-09 10:47:42 -0700

    (feat in progress)pseudocode for technologyController

--- server/technologies/technologyController.js
+++ server/technologies/technologyController.js
@@ -1,4 +1,9 @@
-var technologyController = require('./technologyModel');
+// require necessary models from synced db
+var models = require('../db/models');
+
+// get models specific to our queries
+var Product = models.Product;
+var Technology = models.Technology;
 
 module.exports = {
   // post request: enter a 'product' and get its tech stack
@@ -7,8 +12,20 @@ module.exports = {
     console.log(req.body.searchString);
     
     // get search terms
-    // use search terms to locate companies w/name matching regex string
-    // -- need a db query
-    // send result of db query
+    var searchString = req.body.searchString;
+    // if empty string, return empty array
+    if (searchString === '') {
+      res.send(JSON.stringify([]));
+    }
+    // use search terms to locate products
+    
+    //    - search by url
+    //    - search by product name
+    //    -- need a db query for this ^
+    //    -- **need to update query based on every keystroke
+    //    -- requires wildcard in search query 
+    // use returned results to get tech stack for found companies
+    
+    // send final result of db query
   }
 };
