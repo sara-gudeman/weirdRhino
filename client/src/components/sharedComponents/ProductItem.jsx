2ed910dfe14f4881dd507880517544cef46b3e4b
Harry Sadler <harrysadlermusic@gmail.com>
2015-09-09 17:21:05 -0700

    display user's tech and product follows, edit user API routes

--- client/src/components/sharedComponents/ProductItem.jsx
+++ client/src/components/sharedComponents/ProductItem.jsx
@@ -1,10 +1,15 @@
 var React = require('react/addons');
 
+var Router = require('react-router');
+var Link = Router.Link;
+
+
 var ProductItem = React.createClass({
+
   render: function() {
     return (
       <li>
-        <a href={this.props.link}>{this.props.name}</a>
+        <Link to='product' query={{name: this.props.name}}>{this.props.name}</Link>
       </li>
     );
   }
--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -3,7 +3,7 @@ var UserController = require('./userController');
 module.exports = function(route) {
   route.post('/login', UserController.login);
   route.post('/signup', UserController.signup);
-  route.post('/users', UserController.getUser);
+  route.post('/username', UserController.getUser);
   route.post('/addtech', UserController.addTechToUser);
   route.post('/followproduct', UserController.addProductFollowToUser);
 };
