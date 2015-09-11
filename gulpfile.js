dee786ea3f773d996bdc8f5173f47b00d66700f8
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-09-11 16:58:06 -0700

    travis build task added to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var webpack = require("webpack");
 var nodemon = require('gulp-nodemon');
 var mocha = require('gulp-mocha');
 var env = require('gulp-env');
+var exec = require('child_process').exec;
 
 // gulp.task('default', function() {
   // place code for your default task here
@@ -41,6 +42,14 @@ gulp.task("webpack", function(callback) {
     });
 });
 
+gulp.task('server', function() {
+  exec('node ./server/server.js', function (err, stdout, stderr) {
+    if (err) {
+      console.log('error', err);
+    }
+  });
+})
+
 gulp.task('nodemon', function() {
   nodemon({
     script: "./server/server.js",
@@ -75,6 +84,8 @@ gulp.task('mocha', function() {
 });
 
 gulp.task('default', ['nodemon', 'webpack-watch', 'mocha', 'watch']);
+
+gulp.task('travis', ['server', 'webpack', 'mocha']);
 
 gulp.task('deploy', ["webpack"], function () {
   nodemon({ script: 'server/server.js',
