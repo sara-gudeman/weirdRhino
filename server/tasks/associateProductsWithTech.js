f1f763b6d9005a01e2679739b932b521de465b85
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-11 19:47:49 -0700

    Try catch block to make associate products with tech more robust

--- server/tasks/associateProductsWithTech.js
+++ server/tasks/associateProductsWithTech.js
@@ -14,40 +14,46 @@ module.exports = function(productTechTuple) {
   var products = productTechTuple[0].value();
   var technologies = productTechTuple[1].value();
   return Promise.map(products, function(product) {
-    if(product.value() === null) {
-      console.log("Incorrect or errored out response");
+    try {
+      if(product.value() === null) {
+        console.log("Incorrect or errored out response");
+        return 0;
+      }
+      var productModel = product.value()[0];
+      var productApps = product.value()[1];
+      var appsToAssociate = [];
+      console.log(productModel.product_name, productApps, productModel.favicon_url);
+
+      /**
+       * Keep from executing below associations with
+       * no apps to associate. Would likely throw
+       * and error, but will definitely save a little
+       * time and a DB call
+       */
+      if(!productApps) {
+        return 0;
+      }
+
+      if(productApps.length <= 0) {
+        return 0;
+      }
+
+      for(var i = 0; i < productApps.length; i++) {
+        appsToAssociate.push(technologies.filter(function(tech) {
+          return tech[0].technology_name === productApps[i]
+        })[0]);
+      }
+      productModel.scrape_date = Date.now();
+      for(var i = 0; i < appsToAssociate.length; i++) {
+        appsToAssociate[i] = appsToAssociate[i][0];
+      }
+      productModel.setTechnologies(appsToAssociate);
+      productModel.save();
+      return appsToAssociate.length;
+    
+    } catch (e) {
+      console.log(e.message);
       return 0;
     }
-    var productModel = product.value()[0];
-    var productApps = product.value()[1];
-    var appsToAssociate = [];
-    console.log(productModel.product_name, productApps, productModel.favicon_url);
-
-    /**
-     * Keep from executing below associations with
-     * no apps to associate. Would likely throw
-     * and error, but will definitely save a little
-     * time and a DB call
-     */
-    if(!productApps) {
-      return 0;
-    }
-
-    if(productApps.length <= 0) {
-      return 0;
-    }
-
-    for(var i = 0; i < productApps.length; i++) {
-      appsToAssociate.push(technologies.filter(function(tech) {
-        return tech[0].technology_name === productApps[i]
-      })[0]);
-    }
-    productModel.scrape_date = Date.now();
-    for(var i = 0; i < appsToAssociate.length; i++) {
-      appsToAssociate[i] = appsToAssociate[i][0];
-    }
-    productModel.setTechnologies(appsToAssociate);
-    productModel.save();
-    return appsToAssociate.length;
   });
 }
