1211bfd9aac50db1f39c50d3d8e8b51bb970e600
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-11 19:48:15 -0700

    Try catch block to make the scraper more robust

--- server/tasks/getFavicon.js
+++ server/tasks/getFavicon.js
@@ -9,18 +9,22 @@ module.exports = function(products) {
 }
 
 var getFavicon = function(productModel) {
-  return request.getAsync(productModel[0].product_url)
-    .spread(function(response, body) {
-      var $ = cheerio.load(body);
-      var favicon = $('link[rel="shortcut icon"]').attr('href');
+  try {
+    return request.getAsync(productModel[0].product_url)
+      .spread(function(response, body) {
+        var $ = cheerio.load(body);
+        var favicon = $('link[rel="shortcut icon"]').attr('href');
 
-      if(favicon) {
-        productModel[0].favicon_url = favicon;
-        //productModel.save();
-      }
-      return [productModel];
-    })
-    .catch(function(e) {
-      console.log(e);
-    });
+        if(favicon) {
+          productModel[0].favicon_url = favicon;
+          //productModel.save();
+        }
+        return [productModel];
+      })
+      .catch(function(e) {
+        console.log(e.message);
+      });
+  } catch (e) {
+    console.log("getFavicon Error: ", e.message);
+  }
 };
