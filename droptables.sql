c15dd91877201856eb26ed97e6fee5f2a6b76817
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-12 16:39:45 -0700

    Added script to automate table dropping

---
+++ droptables.sql
@@ -0,0 +1,8 @@
+use stackmatch;
+drop table ProductTechnologies;
+drop table UserProducts;
+drop table UserTechnologies;
+drop table Users;
+drop table Technologies;
+drop table Products;
+drop table Companies;
