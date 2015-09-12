32e4e0376011923bbf54b4ad8535882bcc797511
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-09-11 17:23:53 -0700

    travis test w/o server

--- gulpfile.js
+++ gulpfile.js
@@ -85,7 +85,7 @@ gulp.task('mocha', function() {
 
 gulp.task('default', ['nodemon', 'webpack-watch', 'mocha', 'watch']);
 
-gulp.task('travis', ['server', 'webpack', 'mocha']);
+gulp.task('travis', [webpack', 'mocha']);
 
 gulp.task('deploy', ["webpack"], function () {
   nodemon({ script: 'server/server.js',
