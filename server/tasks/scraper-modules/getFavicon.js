e7f02e02fccb25e4f1b3b096ba4601136099489e
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-14 10:54:57 -0700

    Store absolute path for favicons

--- server/tasks/scraper-modules/getFavicon.js
+++ server/tasks/scraper-modules/getFavicon.js
@@ -1,6 +1,7 @@
 var Promise = require('bluebird');
 var request = Promise.promisifyAll(require('request'));
 var cheerio = require('cheerio');
+var url = require('url');
 
 module.exports = function(products) {
   return Promise.all(products.map(function(product) {
@@ -14,8 +15,14 @@ var getFavicon = function(productModel) {
       .spread(function(response, body) {
         var $ = cheerio.load(body);
         var favicon = $('link[rel="shortcut icon"]').attr('href');
-
         if(favicon) {
+
+          //Make relative urls absolute
+          var parsedFavicon = url.parse(favicon);
+          if(!parsedFavicon.hostname) {
+            favicon = productModel[0].product_url + favicon;
+          }
+
           productModel[0].favicon_url = favicon;
           //productModel.save();
         }
