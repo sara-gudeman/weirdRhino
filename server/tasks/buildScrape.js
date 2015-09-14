4591dbd74e628ca86d128395015c9bb721ea96ca
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-09-14 12:29:38 -0700

    Added dummy scraper task

--- server/tasks/buildScrape.js
+++ server/tasks/buildScrape.js
@@ -1,44 +1,18 @@
-var Promise = require('bluebird');
-var siteQueue = require('./siteBuildQueue');
 var models = require('../db/models');
 var Product = models.Product;
 var Technology = models.Technology;
-var spliceQueueAndProducts = require('./scraper-modules/spliceQueueAndProducts');
-var wappProducts = require('./scraper-modules/wappProducts');
-var toProductModels = require('./scraper-modules/toProductModels');
-var associateProductsWithTech = require('./scraper-modules/associateProductsWithTech');
-var getTechnologies = require('./scraper-modules/getTechnologies');
-var batchProducts = require('./scraper-modules/batchProducts');
-var getFavicon = require('./scraper-modules/getFavicon');
 
-var associations = 0;
-
-spliceQueueAndProducts()
-.then(batchProducts)
-.reduce(function(previousBatch, batch) {
-  associations += previousBatch;
-
-  return Promise.settle(previousBatch)
-  .then(function() {
-    return toProductModels(batch)
-    .settle() 
-    .then(getFavicon)
-    .settle()
-    .then(wappProducts)
-    .settle()
-    .then(getTechnologies)
-    .settle()
-    .then(associateProductsWithTech)
-  });
-}, [Promise.resolve(0)])
-
-.finally(function(finished) {
-  var count = 0;
-  associations = associations.split('');
-  for(var i = 0; i < associations.length; i++) {
-    if(associations[i] == parseInt(associations[i], 10)) {
-      count += parseInt(associations[i], 10);
-    }
-  }
-  console.log("Found " + count + " associations");
+Technology.create({
+  technology_name: "test"
+})
+.then(function(technology){
+  Product.create({
+    product_name: "Test"
+  })
+  .then(function(product) {
+    product.setTechnologies([technology]);
+  })
+})
+.catch(function(err) {
+  console.log(err);
 })
