c21ea2642c0b804720d55a13d09775f925f6ba30
SARA GUDEMAN <sgudeman982@gmail.com>
2015-09-14 21:27:25 -0700

    (feat)adds loading, error, and completion views to modal

--- client/src/components/AddProduct/AddProductModal.jsx
+++ client/src/components/AddProduct/AddProductModal.jsx
@@ -3,12 +3,24 @@ var AddProductForm = require('./AddProductForm');
 var AddProductLoading = require('./AddProductLoading');
 var AddProductError = require('./AddProductError');
 
+var ProductActionCreators = require('../../actions/ProductActionCreators');
+
 var AddProductModal = React.createClass({
+
+  getInitialState: function() {
+    return { loading: false, error: false };
+  },
+
   handleUrlSubmit: function(event) {
     // need to add ajax request here
     // ajax request requires urls to use http://
     event.preventDefault();
-    var userInput = React.findDOMNode(this.refs.urlInput).value;
+    var userInput = React.findDOMNode(this.refs.urlForm.refs.urlInput).value;
+    console.log(userInput);
+    this.setState({ 
+      loading: true
+    });
+    
     $.ajax({
       url: 'api/products/add',
       type: 'POST',
@@ -17,19 +29,26 @@ var AddProductModal = React.createClass({
       },
       dataType: 'json',
       success: function(data) {
+        this.setState({ loading: false, error: false });
         console.log('entered handleUrlSubmit');
-      },
+      }.bind(this),
       error: function(xhr, status, err) {
+        this.setState({ loading: false, error: true });
         console.log('Error: ', status, ' ', err);
-      },
+      }.bind(this),
       complete: function(xhr, status) {
+        this.setState({ loading: false });
         console.log('completed handleUrlSubmit')
-      }
+      }.bind(this)
     });
-    console.log(React.findDOMNode(this.refs.urlInput).value);
   },
 
   render: function() {
+    var form = <AddProductForm ref="urlForm" handleUrlSubmit={this.handleUrlSubmit} />;
+    var loading = <AddProductLoading />;
+    var error = <AddProductError />;
+    var modalState = this.state.loading ? loading : form;
+    var errorState = this.state.error ? error : null;
     return(
       <div className="modal fade add-product">
         <div className="modal-dialog">
@@ -43,9 +62,8 @@ var AddProductModal = React.createClass({
             </div>
 
             <div className="modal-body">
-              <AddProductForm ref="urlInput" handleUrlSubmit={this.handleUrlSubmit} />
-              <AddProductLoading />
-              <AddProductError />
+              { modalState }
+              { errorState }
             </div>
           </div>
         </div>
